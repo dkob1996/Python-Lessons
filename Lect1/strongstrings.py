86d9ab0fc2afe10ae5d63bd898ef6cd2ebd998f8
dkob1996 <dkob1996@gmail.com>
2023-02-16 21:59:09 +0600

    end of lection 1

--- Lect1/strongstrings.py
+++ Lect1/strongstrings.py
@@ -167,6 +167,7 @@ while condition:
 """""
 
 # examples of while
+"""""
 n = 243
 sum = 0
 while n > 0:
@@ -174,3 +175,135 @@ while n > 0:
     sum += x
     n //= 10
 print(sum)  # 9
+"""""
+
+# while - else
+"""""
+while condition:
+    # operator 1
+    # operator 2
+else:
+    # operator n+1
+    # operator n+2
+
+"""""
+
+# example of while - else construction with break
+"""""
+i = 0
+while i < 5:
+    if i == 3:
+        break       # break is not good for normal programmer, better is a flag parameter
+    i += 1
+else:
+    print('Let us')
+    print('stop')
+print(i)
+"""""
+
+# example while elif with flag
+"""""
+n = int(input())
+flag = True
+i = 2
+while flag:
+    if n % i ==0: # if reminder n/i == 0
+        flag = False
+        print(i)
+    elif i>n //2: # i need to be smaller then n / 2
+        print(n)
+        flag = False
+    i += 1
+"""""
+
+# for
+"""""
+for i in enumeration:
+    # operator 1
+    # operator 2
+    ...
+"""""
+
+# example of for
+"""""
+for i in 1, -2, 3, 14, 5:
+    print(i)
+"""""
+
+# range() function which generate sequence
+"""""
+# range(start number, end number, step(default = 1))
+
+r = range(5) # 0 1 2 3 4 
+r = range(2,5) # 2 3 4
+r = range(0,-5) # ----
+r = range(1,10,2) # 1 3 5 7 9  
+r = range(100, 0, -20) # 100 80 60 40 20
+
+for i in r:
+    print(i)
+    # 100 80 60 40 20
+"""""
+
+# for with range()
+"""""
+for i in 'qwerty':
+    print(i)
+
+# q
+# w
+# e
+# r
+# t
+# y
+
+"""""
+
+# example for with range()
+
+a = 'qwery'
+"""""
+print(a[0])
+"""""
+"""""
+for i in a:
+    print(i)
+"""""
+"""""
+line = ""
+for i in range(5):
+    line = ""
+    for j in range(5):
+        line += "*"
+    print(line)
+"""""
+
+# strings
+"""""
+text = 'eat more THIS softly FRANCHE cakes'
+print(len(text))                             # length of string
+print('more' in text)                        # true or false word in string
+print(text.lower())                          # all words in string small
+print(text.upper())                          # all words in string capital
+print(text.replace('more', 'MORE'))          # edit string (more (before) -> More (now))
+"""""
+
+# working with string
+text = 'eat more THIS softly FRANCHE cakes'
+# print(text[start number letter, end number letter, step])
+print(text[0])                               # e
+print(text[1])                               # a
+print(text[len(text)-1])                     # s
+print(text[-5])                              # c
+print(text[:])                               # eat more THIS softly FRANCHE cakes
+print(text[:2])                              # ea
+print(text[len(text)-2:])                    # es
+print(text[2:9])                             # t more
+print(text[6:-18])                           # re THIS so
+print(text[0:len(text):6])                   # erSlNa
+print(text[::2])                             # etmr HSsfl RNH ae
+
+print(text[-1])                              # count goes of the end of the string # s
+
+text = text[2:9] + text[-5] + text[:2]
+print(text)                                  # t more cea
