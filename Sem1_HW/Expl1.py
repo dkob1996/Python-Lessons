d27c04d2f276dc6d6c8a4e429cd3179068ea6fd3
dkob1996 <dkob1996@gmail.com>
2023-02-18 02:04:46 +0600

    solve first hometask

---
+++ Sem1_HW/Expl1.py
@@ -0,0 +1,34 @@
+'''
+Задача 1: Найдите сумму цифр трехзначного числа.
+
+*Пример:*
+
+123 -> 6 (1 + 2 + 3)
+100 -> 1 (1 + 0 + 0) 
+'''
+
+# Solving with string and while
+'''
+current_string = str(input("Enter your number: "))
+
+lenght_of_string = len(current_string)
+
+print(lenght_of_string)
+
+
+sum =0
+i = 0
+while i < lenght_of_string:
+    sum +=int(current_string[i])
+    i +=1
+
+print(sum)
+'''
+
+# Solving with integer and without cycles
+
+current_number = int(input("Enter your number: "))
+
+result_sum = current_number%10 + current_number%100//10 + current_number%1000//100
+
+print(result_sum)
