f202a70ba39c02b342733adfa2c470a335c1706e
dkob1996 <dkob1996@gmail.com>
2023-02-23 16:57:12 +0600

    add theory.py from lecture 2

---
+++ Lect2/theory.py
@@ -0,0 +1,13 @@
+# lists
+list_1 = [] # create empty list
+list_2 = list()  # create empty list
+list_3 = [7,9,11,13,15,17]  # create not empty list
+print(list_3[0])   # print element 0 in list 3  = 7
+print(list_3)      # print all list 3
+
+for i in list_3:   #print each element of list 3
+    print(i)
+
+print(len(list_3))  # count of elements in list 3
+
+print(list_3[-1])   # print last element in list 3
