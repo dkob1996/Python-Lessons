2a6fb0e1d5697dbeef9d1e01c72d549b020feced
dkob1996 <dkob1996@gmail.com>
2023-03-05 16:12:49 +0600

    create 0-2 days in hackerRank 30days tutor

---
+++ HackerRank/Day1/1.py
@@ -0,0 +1,14 @@
+i = 4
+
+a = int(input('Enter int: '))
+b = float(input('Enter float: '))
+s = input('Enter string: ')
+
+a +=i
+print(a)
+
+b +=i
+print(b)
+
+s ='HackerRank '+ s
+print(s)
---
+++ HackerRank/Day2/2.py
@@ -0,0 +1,29 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'solve' function below.
+#
+# The function accepts following parameters:
+#  1. DOUBLE meal_cost
+#  2. INTEGER tip_percent
+#  3. INTEGER tax_percent
+#
+
+def solve(meal_cost, tip_percent, tax_percent):
+    # Write your code here
+    tip = float((meal_cost/100)*tip_percent)
+    tax = float((tax_percent/100)*meal_cost)
+    total_cost = meal_cost+tip+tax
+    print(round(total_cost))
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
